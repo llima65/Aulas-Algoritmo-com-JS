ef166f87d7bf7918bb2f81a7939a261238cc37c7
Lucasli6538 <121589431+llima65@users.noreply.github.com>
2023-10-23 16:17:08 -0300

    Create valorSalada.js

---
+++ valorSalada.js
@@ -0,0 +1,7 @@
+console.log('Salada')
+let Ptomate = 1.25;
+let Ppalmito = 1.50;
+let Pazeitona = 3.50;
+let Ppepino = 1.00;
+let vt = Ptomate + (2*Ppalmito) + (3 *Pazeitona) + (0.5*Ppepino);
+console.log('O valor da salada é R$',vt)
